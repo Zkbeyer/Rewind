ce61b894ad5b60ed7af6aae5e9f41d7ae75b00a3
Zack Beyer <zkbeyer@yahoo.com>
2025-04-19 22:07:43 -0500

    Changed addresses to new deployed site

--- rewind-client/src/Login.jsx
+++ rewind-client/src/Login.jsx
@@ -1,6 +1,6 @@
 function Login() {
     return (
-        <a href="https://ee04-161-130-189-252.ngrok-free.app/login">
+        <a href="https://rewind-api.onrender.com/login">
           <button>Login with Spotify</button>
         </a>
     );
